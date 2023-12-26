68dc56e706d5ab793cba75fb470878648f0f9294
Antonio Pires <tonan@outlook.com.br>
2023-12-26 16:37:05 -0300

    6.1. Testes parametrizados

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Random;
 
@@ -104,4 +106,12 @@ class SaudacaoUtilTest {
         //Assert
         assertDoesNotThrow(chamadaValidaDeMetodo);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5,6,7,8,9,10,11})
+    public void Dado_horario_matinal_Quando_saudar_Entao_deve_retornar_bom_dia(int hora) {
+        String saudacao = saudar(hora);
+        assertEquals("Bom dia", saudacao);
+    }
+
 }
